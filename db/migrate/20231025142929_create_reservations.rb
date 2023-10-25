8a9cbb0cb833fc8eea3a2fd108e4e242e5aa4f66
Cosmas Wasswa <cosywas@gmail.com>
2023-10-25 17:32:49 +0300

    Add reservations table

---
+++ db/migrate/20231025142929_create_reservations.rb
@@ -0,0 +1,11 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.string :city
+      t.date :date
+      t.references :user, foreign_key: true
+      t.references :doctor, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_130508) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_25_142929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_130508) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.string "city"
+    t.date "date"
+    t.bigint "user_id"
+    t.bigint "doctor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_reservations_on_doctor_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "doctors"
+  add_foreign_key "reservations", "users"
 end
