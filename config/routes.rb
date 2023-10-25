cbba7a9970c935f476d1047eea1f902c1f3b6ddb
kayjeee <kagiso.killagram@gmail.com>
2023-10-25 16:32:33 +0200

    Add reservation and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post 'sign_up', to: 'users#sign_up'
+  post 'login', to: 'users#login'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :reservations
+      end
+      resources :doctors
+    end
+  end
 end
