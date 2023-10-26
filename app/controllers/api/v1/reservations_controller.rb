75b94653505f70ef1ded2f8f46dd09bb3f92088f
kayjeee <kagiso.killagram@gmail.com>
2023-10-26 13:14:32 +0200

    updated reservation_controllere create method

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,43 +1,57 @@
 class Api::V1::ReservationsController < ApplicationController
+  # GET /api/v1/users/:user_id/reservations
   def index
     reservations = current_user.reservations
     render json: reservations
   end
 
-  def create
-    existing_reservation = current_user.reservations.find_by(reservation_params)
-    if existing_reservation
-      render json: { status: 'error', message: 'Reservation Already exists' }, status: :unprocessable_entity
+  # POST /api/v1/users/:user_id/reservations
+  # Create action: handles the creation of a new reservation for the current user.
+def create
+  # Check if a reservation with the provided parameters already exists for the current user
+  existing_reservation = current_user.reservations.find_by(reservation_params)
+  
+  if existing_reservation
+    # If an existing reservation is found, return an error response
+    render json: { status: 'error', message: 'Reservation already exists' }, status: :unprocessable_entity
+  else
+    # If no existing reservation is found, create a new reservation for the current user
+    @reservation = current_user.reservations.new(reservation_params)
+
+    if @reservation.save
+      # If the reservation is successfully saved, return a success response
+      render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
     else
-
-      @reservation = current_user.reservations.new(reservation_params)
-
-      if @reservation.save
-        render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
-      else
-        puts @reservation.errors.full_messages
-        render json: { error: 'Unable to create reservation.' }, status: :unprocessable_entity
-      end
+      # If there are errors preventing the reservation from being saved, return an error response
+      render json: { status: 'error', message: @reservation.errors.full_messages.to_sentence,
+                     errors: @reservation.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
+end
+  
 
+  # GET /api/v1/users/:user_id/reservations/:id
   def show
     reservation = current_user.reservations.find(params[:id])
     render json: reservation
   end
 
+  # PUT/PATCH /api/v1/users/:user_id/reservations/:id
   def update
-    # Code to handle PUT/PATCH request to update a reservation
+    @reservation = current_user.reservations.find(params[:id])
+
     if @reservation.update(reservation_params)
       render json: @reservation
     else
-      render json: @reservation.errors, status: :unprocessable_entity
+      render json: { error: 'Unable to update reservation.' }, status: :unprocessable_entity
     end
   end
 
+  # DELETE /api/v1/users/:user_id/reservations/:id
   def destroy
-    # Code to handle DELETE request to delete a reservation
-    @reservation = Reservation.find_by(id: params[:id])
+    @reservation = current_user.reservations.find_by(id: params[:id])
+
     if @reservation
       if @reservation.destroy
         render json: { message: 'Reservation deleted successfully.' }
@@ -51,6 +65,7 @@ class Api::V1::ReservationsController < ApplicationController
 
   private
 
+  # Whitelist parameters for reservation creation and update
   def reservation_params
     params.require(:reservation).permit(:check_in, :check_out, :room_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define custom routes for user authentication
   post 'sign_up', to: 'api/v1/users#sign_up'
   post 'login', to: 'api/v1/users#login'
-  
+
   namespace :api do
     namespace :v1 do
+      # Nested resources for users and their reservations
       resources :users, only: [] do
         resources :reservations
       end
+      # Additional resources, like doctors, can be added here if needed
       resources :doctors
     end
   end
