897f186bc1187e021ad39eb6eae2a1b3ea9322e6
kayjeee <kagiso.killagram@gmail.com>
2023-10-26 13:18:13 +0200

    fixed routes

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -7,29 +7,28 @@ class Api::V1::ReservationsController < ApplicationController
 
   # POST /api/v1/users/:user_id/reservations
   # Create action: handles the creation of a new reservation for the current user.
-def create
-  # Check if a reservation with the provided parameters already exists for the current user
-  existing_reservation = current_user.reservations.find_by(reservation_params)
-  
-  if existing_reservation
-    # If an existing reservation is found, return an error response
-    render json: { status: 'error', message: 'Reservation already exists' }, status: :unprocessable_entity
-  else
-    # If no existing reservation is found, create a new reservation for the current user
-    @reservation = current_user.reservations.new(reservation_params)
+  def create
+    # Check if a reservation with the provided parameters already exists for the current user
+    existing_reservation = current_user.reservations.find_by(reservation_params)
 
-    if @reservation.save
-      # If the reservation is successfully saved, return a success response
-      render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
+    if existing_reservation
+      # If an existing reservation is found, return an error response
+      render json: { status: 'error', message: 'Reservation already exists' }, status: :unprocessable_entity
     else
-      # If there are errors preventing the reservation from being saved, return an error response
-      render json: { status: 'error', message: @reservation.errors.full_messages.to_sentence,
-                     errors: @reservation.errors.full_messages },
-             status: :unprocessable_entity
+      # If no existing reservation is found, create a new reservation for the current user
+      @reservation = current_user.reservations.new(reservation_params)
+
+      if @reservation.save
+        # If the reservation is successfully saved, return a success response
+        render json: { status: 'Success', message: 'Reservation created successfully' }, status: :created
+      else
+        # If there are errors preventing the reservation from being saved, return an error response
+        render json: { status: 'error', message: @reservation.errors.full_messages.to_sentence,
+                       errors: @reservation.errors.full_messages },
+               status: :unprocessable_entity
+      end
     end
   end
-end
-  
 
   # GET /api/v1/users/:user_id/reservations/:id
   def show
