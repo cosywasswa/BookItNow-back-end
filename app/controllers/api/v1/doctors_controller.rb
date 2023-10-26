f01874e95d1dd022be437bc14405604be5dd30f1
clin20n3mun <munanaclinton@gmail.com>
2023-10-26 15:18:17 +0200

    Fix linter errors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,32 +1,37 @@
 class Api::V1::DoctorsController < ApplicationController
-    def index
-      @doctors = Doctor.all 
-      render json: @doctors
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
+
+  def show
+    @doctor = Doctor.find_by(params[:name])
+    render json: @doctor
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render json: {
+        status: { code: 200, message: 'Added doctor successfully', data: @doctor }
+      }, status: :ok
+    else
+      render json: {
+        status: { code: 400, message: 'Failed to add doctor', errors: @doctor.errors.full_messages }
+      }, status: :bad_request
     end
-    def show
-      @doctor = Doctor.find_by(params[:name])
-      render json: @doctor
-    end
-    def create
-        @doctor = Doctor.new(doctor_params)
-      
-        if @doctor.save
-          render json: {
-            status: { code: 200, message: 'Added doctor successfully', data: @doctor }
-          }, status: :ok
-        else
-          render json: {
-            status: { code: 400, message: 'Failed to add doctor', errors: @doctor.errors.full_messages }
-          }, status: :bad_request
-        end
-    end
-    def destroy
-        @doctor = Doctor.find(params[:id])
-        @doctor.destroy
-        head :no_content
-    end
-    private
-    def doctor_params
-        params.require(:doctor).permit(:name, :image, :specialization, :bio, :fee)
-    end
+  end
+
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    @doctor.destroy
+    head :no_content
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :image, :specialization, :bio, :fee)
+  end
 end
