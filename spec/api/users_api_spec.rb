b46a3f315a84eb5bf05d8605c062ce50ff910389
Cosmas Wasswa <cosywas@gmail.com>
2023-11-01 15:35:48 +0300

    Fix linter bugs

--- spec/api/users_api_spec.rb
+++ spec/api/users_api_spec.rb
@@ -1,11 +1,11 @@
 require 'swagger_helper'
 
 describe 'Users API' do
-    path '/sign_up'do
+  path '/sign_up' do
     post 'create new user' do
-        tags 'Users'
-        consumes 'application/json'
-        parameter name: :user, in: :body, schema: {
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
           name: { type: :string }
@@ -14,8 +14,8 @@ describe 'Users API' do
       }
 
       response '200', 'User created successfuly' do
-      let(:user) {{name: 'Martino'}}
-      run_test!
+        let(:user) { { name: 'Martino' } }
+        run_test!
       end
 
       response '422', 'validation error' do
@@ -23,29 +23,28 @@ describe 'Users API' do
         run_test!
       end
     end
+  end
+  path '/login' do
+    post 'Log in a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ['name']
+      }
 
-    end
-    path '/login' do
-        post 'Log in a user' do
-          tags 'Users'
-          consumes 'application/json'
-          parameter name: :user, in: :body, schema: {
-            type: :object,
-            properties: {
-              name: { type: :string }
-            },
-            required: ['name']
-          }
-    
-          response '200', 'user logged in successfully' do
-            let(:user) { { name: 'Martino'} }
-            run_test!
-          end
-    
-          response '404', 'user not found' do
-            let(:user) { { name: 'NonExistentUser' } }
-            run_test!
-          end
-        end
+      response '200', 'user logged in successfully' do
+        let(:user) { { name: 'Martino' } }
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:user) { { name: 'NonExistentUser' } }
+        run_test!
       end
     end
+  end
+end
