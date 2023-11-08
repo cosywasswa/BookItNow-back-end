5b8a48072453c0f4deb41384b678652f0c4737bd
kayjeee <kagiso.killagram@gmail.com>
2023-11-08 14:56:13 +0200

    removed comments on line 16 routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,13 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'api/v1/users#sign_up'
   post 'login', to: 'api/v1/users#login'
 
-  # Nested routes for users and their reservations
   namespace :api do
     namespace :v1 do
       resources :users do
         resources :reservations
       end
       resources :doctors
-      # Add more resources for other controllers if needed
+      
     end
   end
 end
