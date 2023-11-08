15e606219e26fa39c80dc18ecad370646ae684d1
kayjeee <kagiso.killagram@gmail.com>
2023-11-08 14:56:58 +0200

    remove comments in routes line 5

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Routes for user sign up and login
   post 'sign_up', to: 'api/v1/users#sign_up'
   post 'login', to: 'api/v1/users#login'
 
